671a205f2b145bca419fae9053c43c368e451308
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-07 15:55:06 +0000

    added attributes only

--- src/models.py
+++ src/models.py
@@ -11,43 +11,76 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "User"
     #Columns
+    ID = Column()
+    username = Column()
+    password = Column()
+    email = Column()
 
     #Relationships
 
-#Users can save their fav planets and characters
+#Users can save their fav planets and characters, BRIDGE?
 class Favorite(Base):
     __tablename__ = "Favorite"
     #Columns
+    ID = Column()
+    user_id = Column() #foreignkey user
+    planet_id = Column() #foreignkey planet
+    character_id = Column() #foreignkey character
 
     #Relationships
 
 class Character(Base):
-    __tablename__ = "Name"
+    __tablename__ = "Character"
     #Columns
+    ID = Column()
+    name = Column()
+    homeplanet_id = Column() #foreignkey planet
+    starships_id = Column() #foreignkey starship
+    birth_year = Column()
 
     #Relationships
     
 class Planet(Base):
     __tablename__ = "Planet"
     #Columns
+    ID = Column()
+    name = Column()
+    climate = Column()
+    terrain = Column()
+    residents_id = Column() #Foreignkey characters
 
     #Relationships
 
 class Starship(Base):
     __tablename__ = "Starship"
     #Columns
+    ID = Column()
+    name = Column()
+    starship_class = Column()
+    pilots_id = Column() #foreignkey characters
 
     #Relationships
 
 class Specie(Base):
     __tablename__ = "Specie"
     #Columns
+    ID = Column()
+    name = Column()
+    classification = Column()
+    homeplanet_id = Column() #foreignkey planet
+    people_id = Column() #foreignkey characters
 
     #Relationships
 
 class Films(Base):
     __tablename__ = "Films"
     #Columns
+    ID = Column()
+    title = Column()
+    episode_id  = Column()
+    director = Column()
+    characters_id = Column() #foreignkey characters
+    species_id = Column() #foreignkey specie
 
     #Relationships
 
